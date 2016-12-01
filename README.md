f6275fb55c0b618eb5479dbc3f1a165bfe930a6e
Pedro Henrique <system.pedrohenrique@gmail.com>
2016-12-01 14:05:36 -0200

    Better code, update readme, changes to sleep time's.

--- README.md
+++ README.md
@@ -2,6 +2,23 @@
 ---
 Simple PHP script to download PluralSight courses.
 
-You have to edit the `download.php` file with your login information.
+This script simulate a real user watching the courses to avoid getting your account banned.
+**Pluralsight will ban you** if you make too many requests or with no interval between the requests.
 
-To run just call `composer install` and then `php download.php` on the command line.
+To avoid this problem the script will by default download each video file and wait a few seconds.
+You can set the wait time on the **$config** array, the default value is **full** which will wait the full length of the video, you can set the value to: full (the full video length), half (half the video length) or a number.
+
+The default message from Pluralsight when banning you is that you have made a lot of requests with a interval less than 40 seconds.
+I have tried to set the interval to 60 seconds and I was still got banned after a few days.
+
+*You have to edit the `pluralsight.php` file with your login information.*
+
+### How to use
+Before you can use this script you have to install the dependencies.
+From terminal run `composer install`.
+
+You must run this script from the terminal `php pluralsight.php`, make sure you have changed the user and password.
+Put the courses links on the file `download.txt`, one course per line.
+
+
+**Use it at your own risk**
---
+++ pluralsight.php
@@ -0,0 +1,353 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+if (php_sapi_name() != 'cli') {
+    die("You must run this script from the terminal: php pluralsight.php\n");	
+}
+
+/**
+* download_time
+*   full - the clip duration, sleep until time is reached
+*   half - half the clip duration
+*   number - the number duration
+* If the number is less than 30-40 seconds it's very likely the account will be banned
+*
+* download_file
+*	name of the file with the courses link's
+*
+* download_folder
+*	folder to put the download courses 
+**/
+
+$config = array('user' => 'USER GOES HERE', 
+                'password' => 'PASSWORD GOES HERE',
+                'download_folder' => 'Courses',
+                'download_file' => 'download.txt', 
+                'download_time' => 'full');
+
+class PluralSight {
+	const BASE_URL = 'http://app.pluralsight.com';
+	const INFO_URL = 'http://app.pluralsight.com/data/course/';
+	const CONTENT_DATA_URL = 'http://app.pluralsight.com/data/course/content/';
+	const CLIP_URL = 'http://app.pluralsight.com/training/player?';
+	const MAX_GET_TRIES = 8;
+
+    private $config;
+    private $courses;
+    private $session;
+
+    public function __construct($config) {
+        $this->config = $config;    
+    }
+
+    public function run() {
+    	$this->courses = file_get_contents($this->config['download_file']);
+    	$this->courses = explode("\n", $this->courses);
+
+    	$this->CreateFolder($this->config['download_folder']);
+    	chdir($this->config['download_folder']);
+
+    	$this->InitSession();
+
+    	foreach ($this->courses as $course) {
+    		$this->DownloadCourse($course);
+    	}
+    }
+
+    private function InitSession() {
+    	$headers = array('User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/601.5.17 (KHTML, like Gecko) Version/9.1 Safari/601.5.17');
+
+		$data = array('RedirectUrl' => '', 'Username' => $this->config['user'], 
+					  'Password' => $this->config['password'], 'ShowCaptcha' => 'False', 
+					  'ReCaptchaSiteKey' => '6LeVIgoTAAAAAIhx_TOwDWIXecbvzcWyjQDbXsaV');
+
+
+		$this->session = new Requests_Session(self::BASE_URL);
+		$this->session->useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36';
+		$this->session->data = $data;
+
+		$response = $this->session->post('/id');
+    }
+
+    private function DownloadCourse($course) {
+    	if (!$this->IsValidPluralSightURL($course)) {
+    		printf("Invalid PluralSight URL: %s\n", $course);
+    		return;
+    	}
+
+    	$courseID = $this->GetCourseIDFromURL($course);
+    	$courseInfo = $this->GetCourseInfo($courseID);
+
+    	if ($courseInfo->status_code != 200) {
+    		printf("Error, status code %d returned while requesting course info for %s\n", $courseInfo->status_code, $course);
+    		return;
+    	}
+
+    	$courseDescription = "";
+
+    	$courseInfo = json_decode($courseInfo->body);
+    	
+    	$courseDescription .= "Title: " . $courseInfo->title . "\n";
+    	$courseDescription .= "Level: " . $courseInfo->level . "\n";
+    	$courseDescription .= "Duration: " . $courseInfo->duration . "\n";
+    	$courseDescription .= "Release Date: " . $courseInfo->releaseDate . "\n";
+    	$courseDescription .= "Name: " . $courseInfo->name . "\n";
+
+    	$courseDescription .= "Authors:" . "\n";
+    	foreach ($courseInfo->authors as $author) {
+    		$courseDescription .= $author->firstName . "  " . $author->lastName . "\n";	
+    	}
+		
+		$courseDescription .= "\n";
+
+    	$courseDescription .= "Short Description: " . $courseInfo->shortDescription . "\n";
+    	$courseDescription .= "Description: " . $courseInfo->description . "\n";
+    	$courseDescription .= "\n";
+    	$courseDescription .= "\n";
+
+    	$durationSeconds = 0;
+    	$durationSeconds = preg_replace("/^([\d]{1,2})\:([\d]{2})$/", "00:$1:$2", $courseInfo->duration);
+		sscanf($durationSeconds, "%d:%d:%d", $hours, $minutes, $seconds);
+		$durationSeconds = $hours * 3600 + $minutes * 60 + $seconds;
+
+    	printf("Downloading course %s, expected download duration: ~%s\n", $courseInfo->title, gmdate("H:i:s", $durationSeconds + 360));
+
+    	$title = $this->createFolder($courseInfo->title);
+		chdir($title);
+
+		$courseContent = $this->GetCourseContent($courseID);
+
+		$content = array();
+
+		foreach ($courseContent as $module) {
+
+			$clips = $module->clips;
+			$moduleTitle = $module->title;
+
+			$moduleTitle = $this->createFolder($moduleTitle);
+			chdir($moduleTitle);
+
+			printf("\tModule %s\n", $moduleTitle);
+
+			$courseDescription .= $module->title . "\n";
+			$courseDescription .= "    " . $module->description . "\n";
+
+			foreach($clips as $clip) {
+				$clipTitle = $clip->title;
+				$playerParameters = $clip->playerParameters;
+
+				if (empty($clip->playerParameters)) {
+					continue;
+				}
+
+				$courseDescription .= "    " . $clip->title . " " . $clip->duration . "\n";
+
+				$duration = $clip->duration;
+
+				$duration = preg_replace("/^([\d]{1,2})\:([\d]{2})$/", "00:$1:$2", $duration);
+				sscanf($duration, "%d:%d:%d", $hours, $minutes, $seconds);
+				$duration = $hours * 3600 + $minutes * 60 + $seconds;
+
+				$url = self::CLIP_URL . $clip->playerParameters;
+
+				$timeStart = time();
+
+				$fileDownloaded = $this->DownloadURL($url, $clipTitle);
+
+				if (!$fileDownloaded) {
+					$sleepTime = $this->GetSleepTime($duration, $timeStart);
+
+					if ($sleepTime > 0) {
+						printf("\tSleeping for %d seconds\n", $sleepTime);
+						sleep($sleepTime);
+					}
+				}
+				
+			}
+
+			$courseDescription .= "\n";
+
+			chdir("../"); // get out of the module folder
+		}
+
+		file_put_contents("Course.txt", $courseDescription);
+
+		$sleepTimeModule = $this->GetSleepTime();
+		printf("Sleeping for %d seconds\n\n", $sleepTimeModule);
+		sleep($sleepTimeModule);
+		chdir('../'); // get out of course folder
+
+    }
+
+    private function GetSleepTime($clipDuration = 0, $startTime = 0) {
+    	if ($this->config['download_time'] != 'full' && $this->config['download_time'] != 'half') {
+    		return (is_numeric($this->config['download_time'])) ? $this->config['download_time'] : 30;
+    	}
+
+    	if (empty($clipInfo) && empty($startTime)) {
+    		return 30;
+    	}
+    	
+    	if (empty($startTime)) {
+    		return $clipDuration;
+    	}
+
+    	$currentTime = time();
+
+    	$timePassed = $currentTime - $startTime;
+
+    	if ($timePassed > $clipDuration) {
+    		return 0;
+    	} else {
+    		$sleepTime = $clipDuration - $timePassed;
+			return ($this->config['download_time'] == 'half') ? $sleepTime / 2 : $sleepTime;
+    	}
+    }
+
+    private function DownloadURL($url, $clipTitle) {
+    	$url_ = str_ireplace("http://app.pluralsight.com/training/player?", "", $url);
+
+		$query = array();
+		parse_str($url_, $query);
+
+		$author = $query['author'];
+		$name = $query['name'];
+		$clipID = $query['clip'];
+		$course = $query['course'];
+
+		$displayID = $name . "-" . $clipID;
+
+		$simpleURL = str_ireplace("http://app.pluralsight.com", "", $url);
+
+		$result = $this->session->get($simpleURL);
+
+		$body = $result->body;
+
+		$wideScreenSupport = false;
+		$wideScreenSupport = preg_match("/courseSupportsWidescreenVideoFormats: true,/", $body);
+
+		$qualities = array(
+			'high-widescreen' => array('width' => 1280, 'height' => 720),
+			'high' => array('width' => 1024, 'height' => 768),
+			'medium' => array('width' => 848, 'height' => 640),
+			'low' => array('width' => 640, 'height' => 480),
+		);
+
+		$quality = ($wideScreenSupport) ? $qualities['high-widescreen'] : $qualities['high'];
+		$quality = $quality['width'] . "x". $quality['height'];
+
+		$formats = array("mp4", "webm");
+		$currentFormat = 0;
+
+		$clipPost = array(
+			'a' => $author,
+			'cap' => 'false',
+			'cn' => $clipID,
+			'course' => $course,
+			'lc' => 'en',
+			'm' => $name,
+			'mt' => $formats[$currentFormat],
+			'q' => $quality
+		);
+
+		$this->session->data = $clipPost;
+
+		for ($i = 1; $i < self::MAX_GET_TRIES; $i++) {
+			$result = $this->session->post('/training/Player/ViewClip');
+
+			$downloadLink = $result->body;
+
+			if ($downloadLink == "Bad Request") {
+				printf("\tError, could not get the download link, trying to get another resolution/format in 5 seconds.\n");
+
+				if ($i > 3) {
+					$quality = array_values($qualities)[$i - count($qualities)]['width'] . "x". array_values($qualities)[$i - count($qualities)]['height'];
+
+					$clipPost['q'] = $quality;
+					$currentFormat = 1;
+					$clipPost['mt'] = $formats[$currentFormat];
+
+					$session->data = $clipPost;
+				} else {
+					$quality = array_values($qualities)[$i]['width'] . "x". array_values($qualities)[$i]['height'];
+
+					$clipPost['q'] = $quality;
+					$currentFormat = 0;
+					$clipPost['mt'] = $formats[$currentFormat];
+
+					$session->data = $clipPost;
+				}
+
+				sleep(5);
+			} else {
+				break;
+			}
+		};
+		
+
+		$clipTitle = $this->ConvertNameForWindows($clipTitle);
+
+		if (!file_exists($clipTitle . "." . $formats[$currentFormat])) {
+			echo "\tDownloading file: " . $clipTitle . " \n";
+			file_put_contents($clipTitle . "." . $formats[$currentFormat], fopen($downloadLink, 'r'));
+			return false;
+		} else {
+			echo "\tSkipping: " . $clipTitle . " file already exists.\n";
+			return true;
+		}
+    }
+
+    private function GetCourseContent($courseID) {
+    	$result = $this->session->get(self::CONTENT_DATA_URL . $courseID);
+		$result = json_decode($result->body);
+		return $result;
+    }
+
+    private function GetCourseInfo($courseID) {
+    	$result = $this->session->get(self::INFO_URL . $courseID);
+		return $result;
+    }
+
+    private function ConvertNameForWindows($name) {
+	    if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+			$name = str_ireplace("<", "-", $name);
+			$name = str_ireplace(">", "-", $name);
+			$name = str_ireplace(":", "-", $name);
+			$name = str_ireplace("\"", "'", $name);
+			$name = str_ireplace("/", "-", $name);
+			$name = str_ireplace("\\", "-", $name);
+			$name = str_ireplace("|", "-", $name);
+			$name = str_ireplace("?", " ", $name);
+			$name = str_ireplace("*", " ", $name);
+		}
+
+		return $name;
+    }
+
+    private function CreateFolder($name) {
+    	$name = $this->ConvertNameForWindows($name);
+	
+		if (!is_dir($name)) {
+		    mkdir($name, 0777, true);
+		}
+
+		return $name;
+    }
+
+    private function IsValidPluralSightURL($url) {
+    	$regex = "/(http|https):\\/\\/app\\.pluralsight.com\\/library\\/courses\\/[a-zA-Z0-9\\-_]+/";
+		$result = preg_match($regex, $url);
+		return $result;
+    }
+
+    private function GetCourseIDFromURL($courseUrl) {
+    	$course = str_ireplace("https://app.pluralsight.com/library/courses/", "", $courseUrl);
+		$course = str_ireplace("/table-of-contents", "", $course);
+		return $course;
+    }
+
+}
+
+$app = new PluralSight($config);
+$app->run();
